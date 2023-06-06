81d03f353fff39594a32a2c0c40e78ad27ee493d
George Withers <george.withers@gmail.com>
2023-06-06 17:54:00 +0000

    Added s3 bucket module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "george-withers-org-workshop"
     workspaces {
       name = "hashicat-aws"
